fc8f754df65df0a0ec0eb60ca461d0cf144716e7
Dunn <hello@origamijoji.com>
2026-03-11 18:33:19 -0700

    fix: remove pin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,9 @@
 return {
   {
-    "catppuccin/nvim",
-    tag = "v1.10.0", -- LazyVim/LazyVim issue #6355
-    opts = {
-      flavour = "macchiato",
-    },
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
   },
 }
+
